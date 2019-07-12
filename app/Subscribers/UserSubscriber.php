9ca33b7ebb06552ab5a846e4881bd9c32e0d3648
linvanda <1049180458@qq.com>
2019-07-12 14:58:13 +0800

    change

--- app/Subscribers/UserSubscriber.php
+++ app/Subscribers/UserSubscriber.php
@@ -3,7 +3,9 @@
 namespace App\Subscribers;
 
 use App\Domain\Events\UserAddedEvent;
+use App\Domain\Events\UserMergedEvent;
 use App\Domain\Events\UserUpdatedEvent;
+use App\Domain\User\User;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -21,17 +23,40 @@ class UserSubscriber implements EventSubscriberInterface
             ],
             UserUpdatedEvent::class => [
                 ['userUpdated']
+            ],
+            UserMergedEvent::class => [
+                ['userMerged']
             ]
         ];
     }
 
     public function userAdded(UserAddedEvent $event)
     {
-        //
+        // TODO 初始化用户等级
+
+        // TODO 初始化卡、积分
     }
 
     public function userUpdated(UserUpdatedEvent $event)
     {
-        //
+        $this->changeLog($event->oldUser(), $event->newUser(), 1);
+    }
+
+    public function usrMerged(UserMergedEvent $event)
+    {
+        $this->changeLog($event->abandonUser(), $event->targetUser(), 2);
+
+        // TODO 通知其他子系统
+    }
+
+    /**
+     * 记录变更日志
+     * @param User $oldUser
+     * @param User $newUser
+     * @param int $changeType 1 update, 2 merge
+     */
+    private function changeLog(User $oldUser, User $newUser, int $changeType)
+    {
+        // TODO 记录变更日志
     }
 }
